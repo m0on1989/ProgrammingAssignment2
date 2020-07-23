a394dffd90d5e20ae5305a42b1a89614bc838638
m0on1989 <68518373+m0on1989@users.noreply.github.com>
2020-07-23 18:33:23 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+R Programming Project 2
 
-## Write a short comment describing this function
-
+Function:
 makeCacheMatrix <- function(x = matrix()) {
-
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
+
+Testing my function:
+
+> pmatrix <-makeCacheMatrix(matrix(1:16, nrow=4, ncol=4))
+> pmatrix$get()
+     [,1] [,2] [,3] [,4]
+[1,]    1    5    9   13
+[2,]    2    6   10   14
+[3,]    3    7   11   15
+[4,]    4    8   12   16
+> pmatrix$getInverse()
+NULL
+> pmatrix <-makeCacheMatrix(matrix(1:4, nrow=2, ncol=2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> pmatrix$getInverse()
+NULL
+> cacheSolve(pmatrix)
+getting cached data
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> pmatrix$getInverse()
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> pmatrix$set(matrix(c(2,2,1,4),2,2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    2    1
+[2,]    2    4
+> pmatrix$getInverse()
+NULL
+> cacheSolve(pmatrix)
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> cacheSolve(pmatrix)
+getting cached data
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+> pmatrix$getInverse()
+           [,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+
+> 
